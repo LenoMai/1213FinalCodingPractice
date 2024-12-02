e4928bf65bbb0465a8ff55f9cd6cbf6849b546a8
Jacob Teddy <tetraphobia@wyvrn.dev>
2024-12-02 18:10:00 -0500

    refactor: drop try/catch blocks and let methods throw exceptions

--- src/Prob1/txtHandler.java
+++ src/Prob1/txtHandler.java
@@ -2,40 +2,28 @@ package Prob1;
 
 import java.io.*;
 
-public class txtHandler implements FileHandler{
+public class txtHandler implements FileHandler {
 
-    public txtHandler(){}
-    
+    public txtHandler() {
+    }
+
     @Override
     public void readFile(File filename) throws IOException {
         BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            throw new IOException("Error reading TXT file: " + e.getMessage());
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
+        reader = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
         }
+
+        reader.close();
     }
 
     @Override
     public void writeData(File filename, String data) throws IOException {
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(filename));
-            writer.write(data);
-        } catch (IOException e) {
-            throw new IOException("Error writing to TXT file: " + e.getMessage());
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
-        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write(data);
+
+        writer.close();
     }
 }
